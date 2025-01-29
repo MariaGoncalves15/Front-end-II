d09bf24d47c0da01f440c6444a2fe38d4e97f443
MariaGoncalves15 <maria.goncalves@estudante.ifro.edu.br>
2025-01-29 08:26:05 -0400

    subirprogit

--- projeto/src/app/efeitos/page.js
+++ projeto/src/app/efeitos/page.js
@@ -9,6 +9,8 @@ export default function Efeitos() {
     // })
     const [ufs, setUfs] = useState([]);
     const [ufSeleted, setufSeleted] = useState('');
+    const [cities, setCities] = useState([]);
+    const [citySelected, setCitySelected] = useState('');
 
     const getUfs = async () => {
         try{
@@ -25,24 +27,57 @@ export default function Efeitos() {
         
     }
 
+    const getCities = async () => {
+        try{
+            const response = await fetch (`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufSeleted}/municipios?orderBy=nome`);
+            if(!response.ok) {
+                throw new Error('Erro ao buscar dados: ' +response.statusText);
+            }
+            const data = await response.json();
+            setCities(data);
+            console.log(data);
+        }catch(error){
+            console.log('Ocorreu algum erro: ' + error)
+        }
+    }
+
     useEffect(() => {
         getUfs();
     },[])
 
-
+    useEffect(() => {
+        getCities();
+    },[ufSeleted])
     return (
         <div>
             <h1>Efeitos</h1>
         {
-            <select onChange={(ev)=> setufSeleted(ev.target.value)}>
+            <select onChange={(ev)=> {setufSeleted(ev.target.value), setCitySelected('')}}>
                 <option value="" >Selecione o estado</option>
                 {ufs.map((uf) => (
-                    <option value={uf.id} key={uf.id}>
-                        {uf.nome}
+                    <option 
+                    value={uf.id} 
+                    key={uf.id}>
+                    {`${uf.nome} - ${uf.sigla}`}
                     </option>
                 ))}
             </select>
         }
+
+        {
+            <select onChange={(ev)=> setCitySelected(ev.target.value)}>
+                <option value="" >Selecione a cidade</option>
+                {cities.map((city) => (
+                    <option 
+                    value={city.nome} 
+                    key={city.id}>
+                    {`${city.nome}`}
+                    </option>
+                ))}
+            </select>
+        }
+        {citySelected?<p>{citySelected}</p>:<p>Aguardando!</p>}
+
             {/* <button onClick={() => (setCont1(cont1+1))}>Adicionar</button>
             <p>Renderizações cont 1: {cont1}</p>
             <button onClick={() => (setCont2(cont2+1))}>Adicionar</button>
