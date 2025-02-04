9be56d64819fa1e6054f9387d7f4e1b6cb2fbdde
MariaGoncalves15 <maria.goncalves@estudante.ifro.edu.br>
2025-02-04 10:06:23 -0400

    salvarprojeto

---
+++ projeto_02/src/app/components/Header/index.js
@@ -0,0 +1,14 @@
+'use client';
+import Link from "next/link";
+import style from "./header.module.css";
+import Image from "next/image";
+
+export default function Header() {
+    return (
+        <header style= {{
+            width: '100%', 
+            background: '#'
+        }}></header>
+    )
+}
+
