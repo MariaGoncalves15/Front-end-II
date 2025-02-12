46bf59e3bcab8acefc9ddbf81e8380783e22726e
MariaGoncalves15 <maria.goncalves@estudante.ifro.edu.br>
2025-02-11 20:52:44 -0400

    sqalvar

--- projeto_02/.next/static/chunks/webpack.js
+++ projeto_02/.next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("7eca287e011eb8bb")
+/******/ 		__webpack_require__.h = () => ("8a21281c56353fd4")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
