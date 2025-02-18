2db9cd0d22232d6b4d0819f0cc6cb07191280fc0
MariaGoncalves15 <maria.goncalves@estudante.ifro.edu.br>
2025-02-18 18:55:28 -0400

    salvraaa

--- projeto_02/.next/static/chunks/webpack.js
+++ projeto_02/.next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("97317955caac2f02")
+/******/ 		__webpack_require__.h = () => ("293f1fcda6f70c85")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
